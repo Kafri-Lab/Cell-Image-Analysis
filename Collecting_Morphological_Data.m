8f3733a3c52048b078b0e5aa4d5df2251fea9ac0
Heather D'Souza <heather.dsouza@hotmail.com>
2017-06-07 15:46:54 -0400

    commented-out visualization and added cell symmetry values to table

--- Collecting_Morphological_Data.m
+++ Collecting_Morphological_Data.m
@@ -9,25 +9,27 @@ Total_num_of_cells=max(O.BW{Cell_ch}(:));
 [fyx,fyy]=gradient(fy);
 D=fyy.*fxx-fxy.*fyx;
 
-CellSymmetry=imgaussfilt(fxx-fyy,10);
-ShapeSaddle=imgaussfilt(D,10);
-ShapeSaddleNormalized = normalize0to1(ShapeSaddle);
-stats=regionprops(O.BW{Nucleus_ch},ShapeSaddleNormalized,'area','MeanIntensity','Centroid');
+CellSymmetryImage=imgaussfilt(fxx-fyy,10);
+SaddlePointImage=imgaussfilt(D,10);
+SaddlePointImageNormalized = normalize0to1(SaddlePointImage);
+stats=regionprops(O.BW{Nucleus_ch},SaddlePointImageNormalized,'area','MeanIntensity','Centroid');
 
 Centroid_=cat(1,stats.Centroid);
 
 cyt = O.Original_IM{1};
-saddleOverlayImage = ShapeSaddle;
-Mitosis = []; 
+saddleOverlayImage = SaddlePointImage;
+NumberOfCells = max(O.BW{Nucleus_ch}(:));
+SaddlePoint = zeros(1,NumberOfCells)'; 
+CellSymmetry = zeros(1,NumberOfCells)'; 
 
 for a=1:2
-    
-    nuc = O.Original_IM{3};
-    if a==1
-        figTitle = 'Saddle Point Metric';
-    else
-        figTitle = 'Cell Symmetry Metric';
-    end 
+%     NOTE: These comments are helpful for visualization
+%     nuc = O.Original_IM{3};
+%     if a==1
+%         figTitle = 'Saddle Point Metric';
+%     else
+%         figTitle = 'Cell Symmetry Metric';
+%     end 
     
     for n=1:size(Centroid_,1)
         
@@ -35,45 +37,45 @@ for a=1:2
         y = round(Centroid_(n,2));
         
         if a==1
-            value = ShapeSaddle(y,x); 
-            Mitosis = [Mitosis;round(value,2)];
+            value = SaddlePointImage(y,x); 
+            SaddlePoint(n:NumberOfCells,1)=round(value,2);
         else
-            value = abs(CellSymmetry(y,x));
+            value = abs(CellSymmetryImage(y,x));
+            CellSymmetry(n:NumberOfCells,1)=round(value,2);
         end
         
-        value = sprintf('%.2f', value);
-        text = text2im(value);
-        text = text.*double(max(nuc(:)));
-        [sizeY,sizeX] = size(text);
-       
-        if x+1>=size(nuc,1)
-            nuc(y:sizeY+y-1,x-sizeX:x-1)=text;
-        else
-            nuc(y:sizeY+y-1,x:sizeX+x-1)=text;
-        end
+%         value = sprintf('%.2f', value);
+%         text = text2im(value);
+%         text = text.*double(max(nuc(:)));
+%         [sizeY,sizeX] = size(text);
+%        
+%         if x+1>=size(nuc,1)
+%             nuc(y:sizeY+y-1,x-sizeX:x-1)=text;
+%         else
+%             nuc(y:sizeY+y-1,x:sizeX+x-1)=text;
+%         end
         
     end
     
-    figure
-    imshow(nuc, [])
-    title(figTitle);
-    hold on
-    plot(Centroid_(:,1),Centroid_(:,2),'.r')
+%     figure
+%     imshow(nuc, [])
+%     title(figTitle);
+%     hold on
+%     plot(Centroid_(:,1),Centroid_(:,2),'.r')
 end
 
-T = table(Centroid_,Mitosis);
+T = table(SaddlePoint,CellSymmetry);
 
-figure
-imshow(cyt, [])
-hold on
-plot(Centroid_(:,1),Centroid_(:,2),'.r')
+% figure
+% imshow(cyt, [])
+% hold on
+% plot(Centroid_(:,1),Centroid_(:,2),'.r')
 
 % figure
 % imshow(saddleOverlayImage, [])
 % hold on
 % plot(Centroid_(:,1),Centroid_(:,2),'.r')
-save('Mitosis_Probability.mat', 'T');
-pause;
-close all;
+% pause;
+% close all;
 
 
