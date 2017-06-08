e46d9dae978a787ea09869168fcd5e0464b5ddcd
Daniel Snider <danielsnider12@gmail.com>
2017-06-08 13:11:32 -0400

    Lior's processing commands

---
+++ Lior_ImageProcessingCommands.m
@@ -0,0 +1,7 @@
+DirName='\\carbon.research.sickkids.ca\rkafri\OPRETTA\Operetta Processed OutPutFiles\Dataset_20170322_TG_Fibroblast_movie_2RESULTS';
+FileName='SegPar_SegmentationParameters';
+load([DirName '\' FileName])
+Data.O.Collecting_X_Data='Collecting_Morphological_Data';
+Data.O.SegmentationParameters.OutputDir = '\\carbon.research.sickkids.ca\rkafri\Heather\ResultsTables\'
+[ResultTable]=O_SegmentCells_v6(Data);
+
